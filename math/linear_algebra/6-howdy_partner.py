cd880f9b5eda953b9e789b7f719d5f43986e1589
dntwaritag <d.ntwaritag@alustudent.com>
2024-09-12 06:00:55 -0700

    6-howdy_partner.py

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python3
 '''
-    A function def add_matrices2D(mat1, mat2):
-    that adds two matrices element-wise
+    A function def cat_arrays(arr1, arr2):
+    that concatenates two arrays
 '''
 
 
-def add_matrices2D(mat1, mat2):
+def cat_arrays(arr1, arr2):
     '''
-        A function def add_matrices2D(mat1, mat2):
-        that adds two matrices element-wise
+        A function def cat_arrays(arr1, arr2):
+        that concatenates two arrays
     '''
-    if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
-        return [
-            [
-                mat1[i][j] + mat2[i][j]
-                for j in range(len(mat1[0]))
-            ]
-            for i in range(len(mat1))
-        ]
-    else:
-        return None
+    return arr1 + arr2
