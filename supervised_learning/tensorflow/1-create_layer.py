56ef54a37312870632b960a2937afbc6f8497e8d
dntwaritag <d.ntwaritag@alustudent.com>
2025-01-28 06:32:04 -0600

    updates

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-
+""" create layer """
 import tensorflow as tf
 
-create_placeholders = __import__('0-create_placeholders').create_placeholders
 
-x, y = create_placeholders(784, 10)
-print(x)
-print(y)
+def create_layer(prev, n, activation):
+    """ create layer"""
+    W = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    L = tf.layers.Dense(units=n, activation=activation,
+                        kernel_initializer=W, name="layer")
+    return L(prev)
