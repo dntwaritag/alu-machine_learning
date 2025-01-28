aa408316aa35f4c28058bb7d0b10cc686754fe81
dntwaritag <d.ntwaritag@alustudent.com>
2025-01-28 06:53:21 -0600

    updates

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -46,4 +46,3 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
             if i < iterations:
                 sess.run(train_op, feed_dict={x: X_train, y: Y_train})
         return saver.save(sess, save_path)
-     
