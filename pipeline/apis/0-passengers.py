7c421dfecf0b4b5b73cd5ed855f5e10a69bebd53
dntwaritag <d.ntwaritag@alustudent.com>
2025-03-02 04:41:58 -0800

    updates

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,30 @@
+eturn list of ships"""
+
+
+import requests
+
+
+def availableShips(passengerCount):
+    """ Return list of ships
+
+    Args:
+        passengerCount (int): number of ships
+    """
+
+    res = requests.get('https://swapi-api.alx-tools.com/api/starships')
+
+    output = []
+    while res.status_code == 200:
+        res = res.json()
+        for ship in res['results']:
+            passengers = ship['passengers'].replace(',', '')
+            try:
+                if int(passengers) >= passengerCount:
+                    output.append(ship['name'])
+            except ValueError:
+                pass
+        try:
+            res = requests.get(res['next'])
+        except Exception:
+            break
+    return output
