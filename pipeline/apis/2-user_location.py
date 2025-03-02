acf5084a6849f7b235d36ee6d372a0377cb2427f
Denys NTWARITAGANZWA <127384993+dntwaritag@users.noreply.github.com>
2025-03-02 05:06:03 -0600

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -43,7 +43,7 @@ def get_user_location(api_url):
         print(error_msg)
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     if len(sys.argv) != 2:
         print("Usage: ./2-user_location.py <API_URL>")
         sys.exit(1)
